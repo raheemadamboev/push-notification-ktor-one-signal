0c7648e1f35de206eea1ff42a71837a354dee1dd
Raheem <raheemideadev@gmail.com>
2021-11-06 20:28:30 +0500

    main ui

--- app/src/main/java/xyz/teamgravity/pushnotificationktoronesignal/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/pushnotificationktoronesignal/MainActivity.kt
@@ -3,19 +3,57 @@ package xyz.teamgravity.pushnotificationktoronesignal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import xyz.teamgravity.pushnotificationktoronesignal.ui.theme.PushNotificationKTOROneSignalTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PushNotificationKTOROneSignalTheme {
+            var title by remember { mutableStateOf("") }
+            var description by remember { mutableStateOf("") }
 
+            PushNotificationKTOROneSignalTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp),
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    TextField(
+                        value = title,
+                        onValueChange = { title = it },
+                        placeholder = { Text("Title") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    TextField(
+                        value = description,
+                        onValueChange = { description = it },
+                        placeholder = { Text("Description") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text(text = "Send")
+                    }
+                }
             }
         }
     }
